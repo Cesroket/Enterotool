df4520dcaaf489651e62daa12bff4cf9772152e7
cesroket <149941846+Cesroket@users.noreply.github.com>
2024-12-26 14:21:15 +0100

    updated installer

--- Installer.sh
+++ Installer.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 echo "Installing dependencies"
+#the idea is to make Enterotool as a variable in .bashrc
+
+## this is the function that will be added to .bashrc
+function Enterotool() {
+
+if [[ "$1" == "--mkdb" ]]; then
+    echo "making database"
+    python ~/Documents/Enterobaser.sh
+
+elif [[ "$1" == "--h" ]]; then
+    cat /home/cesar/Documents/Enterotool-manual.txt
+else
+    bash ~/Documents/Enterotool.sh "$@"
+
+fi
+}
