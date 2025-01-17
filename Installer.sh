5612e335c7fc7ec77b848eed7dfbb60c00ee6689
cesroket <149941846+Cesroket@users.noreply.github.com>
2025-01-17 10:15:31 +0100

    v.1.0.0

--- Installer.sh
+++ Installer.sh
@@ -1,6 +1,19 @@
 
 #!/bin/bash
 
+echo "Enterotool conda Environment Setup:"
+echo "Hi, this is the installer, If you proceed, it will create a conda enviorment with all the required packages!!"
+echo "If you have all the depencies requiered, you dont need to run this script."
+echo "Make sure you have the following tools installed: seqkit, trimmomatic, vsearch, blastn, matplotlib, pandas"
+echo "Do you want to proceed? (y/n)"
+
+read -r response 
+if [[ ! "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    echo "Exiting..."
+    exit 0
+fi
+
+
 ENV_NAME="enterotool"
 
 if ! command -v conda &> /dev/null; then
@@ -25,6 +38,11 @@ if ! command -v seqkit &> /dev/null || ! command -v trimmomatic &> /dev/null || 
     echo "Some tools were not installed correctly. Please check the logs and try again." >&2
     exit 1
 fi
+
+echo "Make sure you have the database files in the database folder,"
+echo " if not, download them from the Enterotool GitHub repository."
+echo "or made your own database files with blast -makeblastdb command."
+echo "#####################################################"
 echo "Environment setup completed successfully!"
 echo "Activate the environment with: conda activate $ENV_NAME"
 echo "You can now run the Enterotool-pipeline script."
